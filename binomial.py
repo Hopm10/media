8365a41efcd33520d0dc2a505ad1b871f8c9391d
Hopm10 <hopm10999@gmail.com>
2020-10-07 18:34:41 +0700

    update struture folder

---
+++ binomial.py
@@ -0,0 +1,43 @@
+import math
+
+def prob (n,p,N):
+    return (math.comb(N,n)*math.pow(p,n)*math.pow(1-p,N-n))
+# print(prob(3,0.5,6))
+
+def infoMeasure(n,p,N):
+    return (-math.log2(prob(n,p,N)))
+# print(infoMeasure(5,0.5,7))
+
+
+
+'''
+    Function sumProb:
+    ------------------------
+    Thử giá trị với N = 100 hoặc 1000 và p = 1/2 thì sum ~ 1
+    ------------------------  
+'''
+def sumProb(N,p):
+    sum = 0
+    for i in range(1, N+1):
+        sum = sum + prob(i, p, N)
+    return sum
+# print(sumProb(100,0.5))
+
+
+
+'''
+    Function approxEntropy:
+    --------------------------
+
+    Giá trị thực nghiệm N = 100 và p = 1/2 => entropy = 4.369011409223017
+    Giá trị thực nghiệm N = 200 và p = 1/2 => entropy = 4.869020643912809
+
+    --------------------------
+'''
+def approxEntropy(N,p):
+    
+    entropy = 0
+    for i in range(1,N+1):
+        entropy = entropy + prob(i,p,N)*infoMeasure(i,p,N)
+    return entropy
+# print(approxEntropy(500,0.5))
---
+++ geometric.py
@@ -0,0 +1,44 @@
+import math
+def prob (n,p):
+    return (p*(math.pow(1-p,n-1)))
+
+def infoMeasure(n,p):
+    return (-math.log2(prob(n,p)))
+# print(infoMeasure(3,0.5))
+
+
+'''
+    Function sumProb:
+    ------------------------
+    Nếu N tiến đến vô cùng thì sumProb trả về tổng của một cấp số nhân
+    Khi đó giá trị sum = p/(1-q) với |q|< 1
+    Mà 1 - q = p 
+    => giá trị cuối cùng là 1
+    ------------------------
+    
+'''
+def sumProb(N,p):
+    i = 1
+    sum = 0
+    while(i <= N):
+        sum = sum + prob(i,p)
+        i += 1
+    return sum
+# print(sumProb(100,0.5))
+
+
+
+'''
+    Function approxEntropy:
+    --------------------------
+    Giá trị thực nghiệm N = 100 hoặc 1000 và p = 1/2 thì entropy xấp xỉ 1.99999999999999
+    --------------------------
+'''
+def approxEntropy(N,p):
+    i = 1
+    entropy = 0
+    while(i <= N):
+        entropy = entropy + prob(i,p)*infoMeasure(i,p)
+        i += 1
+    return entropy
+print(approxEntropy(10,0.5))
